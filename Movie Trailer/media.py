333bf9cdbc903d0236df4160de3faae055afd72a
Ben <bhargreaves91@gmail.com>
2018-04-06 12:31:30 -0400

    Add files via upload
    Comment / Code styling updates based on reviewer feedback

--- Movie Trailer/media.py
+++ Movie Trailer/media.py
@@ -1,13 +1,16 @@
 class Movie():
     '''
-    Movie Class, used to store useful information about a particular movie,
+    Movie Class, used to store useful information about a particular movie.
+
+    Parameters;
+    title - string: The Title of the movie
+    storyline - string: A brief synopsis of the movies plot
+    poster_image_url - string: A URL link to the movies poster
+    trailer_youtube_url - string: A URL link to the movies youtube trailer
     '''
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, storyline,
+                 poster_image_url, trailer_youtube_url):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-        
-
-        
-        
